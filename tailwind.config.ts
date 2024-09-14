80c22e78445886aedc58f06ca9611a1bd143f9a9
Musa <77996753+MusaAqeel@users.noreply.github.com>
2024-09-14 05:35:04 -0400

    sign

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,9 @@ const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
+/** @type {import('tailwindcss').Config} */
 const config: Config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -72,6 +73,9 @@ const config: Config = {
             transform: "translate(calc(-50% - 0.5rem))",
           },
         },
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
       },
     }
   },
