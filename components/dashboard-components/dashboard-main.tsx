6b26f4cef8ddcbfd6e9010bc31bf61fcb75b1757
Musa <77996753+MusaAqeel@users.noreply.github.com>
2024-09-15 06:25:26 -0400

    Revert "commit"
    This reverts commit ba3eca4d8c9ac333bdb887470aad4d0d3c87693b.

--- components/dashboard-components/dashboard-main.tsx
+++ components/dashboard-components/dashboard-main.tsx
@@ -9,8 +9,14 @@ export function DashboardMain() {
   return (
     <div className="chart-wrapper mx-auto h-full overflow-hidden flex max-w-7xl flex-col flex-wrap items-start justify-center gap-6 pb-6 sm:flex-row sm:pb-8">
       <div className="grid w-full gap-6 sm:grid-cols-2 lg:max-w-[22rem] lg:grid-cols-1 xl:max-w-[25rem]">
+<<<<<<< Updated upstream
+        {/* <GreetingWidget /> */}
+        <NetWorthWidget worth={123456} />
+        <OverviewWidget liquidCash={10000} debt={5000} investedAssets={100000} />
+=======
         <NetWorthWidget />
         <OverviewWidget />
+>>>>>>> Stashed changes
       </div>
       <div className="grid w-full flex-1 gap-6 lg:max-w-[20rem] h-auto overflow-hidden">
         <NewsWidget />
